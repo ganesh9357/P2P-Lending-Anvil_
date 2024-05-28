c31bb831a64f44d9a3a901646a42e2cfe3f307bd
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-28 05:17:09 +0000

    Edited borrower.dashboard.Form1

--- client_code/borrower/dashboard/Form1/__init__.py
+++ client_code/borrower/dashboard/Form1/__init__.py
@@ -22,7 +22,6 @@ class F(FTemplate):
     self.disable_land_farming_details_fields()
     self.disable_borrower_profile_info_fields()
     self.disable_business_details_fields()
-    self.disable_bank_details_fields()
     self.load_user_profile()
     self.disable_company_employment_fields()
 
@@ -88,6 +87,17 @@ class F(FTemplate):
       self.cin_tx.text = user_profile['cin']
       self.office_address_tx.text = user_profile['registered_off_add']
       self.proof_varification.source = user_profile['proof_verification']
+      self.holder_name_tx.text = user_profile['account_name']
+      self.account_no_tx.source = user_profile['account_number']
+      self.brach_name_tx.text = user_profile['account_bank_branch']
+      self.bank_id_tx.text = user_profile['bank_id']
+      self.bank_name_tx.text = user_profile['bank_name']
+      self.acccount_type_dropdown.selected_value = user_profile['account_type']
+
+      options = app_tables.fin_borrower_account_type.search()
+      options_string = [str(option['borrower_account_type']) for option in options]
+      self.acccount_type_dropdown.items = options_string
+      
 
     if borrower_details:
       self.credit_limit_tx.text = borrower_details['credit_limit']
@@ -184,6 +194,8 @@ class F(FTemplate):
       self.enable_personal_fields()
       self.save_personal_details_button.visible = True
       self.edit_personal_details_button.visible = False
+      self.govt_1_file_loader_1.visible = True
+      self.govt_2_file_loader_2.visible = True
 
   def save_personal_details_click(self, **event_args):
     user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
@@ -250,6 +262,8 @@ class F(FTemplate):
     self.enable_company_employment_fields()
     self.save_company_employment_details_button.visible = True
     self.edit_company_employment_details_button.visible = False
+    self.employee_id_image.visible = True
+    self.employee_last_six_bank_state_image.visible = True
 
   def save_company_employment_details_click(self, **event_args):
     user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
@@ -290,6 +304,7 @@ class F(FTemplate):
     self.enable_college_details_fields()
     self.save_college_details_button.visible = True
     self.edit_college_details_button.visible = False
+    self.college_proof_image.visible = True
 
   def save_college_details_click(self, **event_args):
     user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
@@ -389,6 +404,8 @@ class F(FTemplate):
     self.enable_business_details_fields()
     self.save_business_details_button.visible = True
     self.edit_business_details_button.visible = False
+    self.business_bank_st_image.visible = True
+    self.business_proof_varificaions_image.visible = True
 
   def save_business_details_click(self, **event_args):
     user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
@@ -397,7 +414,7 @@ class F(FTemplate):
       user_profile['business_add'] = self.business_address_tex.text
       user_profile['business_type'] = self.business_type_dropdown.selected_value
       user_profile['employees_working'] = self.no_of_emp_dropdown.selected_value
-      user_profile['year_estd'] = self.year_of_yest_tx.text
+      # user_profile['year_estd'] = self.year_of_yest_tx.text
       user_profile['industry_type'] = self.industry_type_dropdown.selected_value
       user_profile['six_month_turnover'] = self.last_six_turnover_tx.text
       user_profile['last_six_month_bank_proof'] = self.last_six_month_for_business.source
@@ -409,96 +426,42 @@ class F(FTemplate):
     self.disable_business_details_fields()
     self.save_business_details_button.visible = False
     self.edit_business_details_button.visible = True
+
+  def disable_bank_details_fields(self):
+    self.holder_name_tx.enabled = False
+    self.account_no_tx.enabled = False
+    self.brach_name_tx.enabled = False
+    self.bank_id_tx.enabled = False
+    self.bank_name_tx.enabled = False
+    self.acccount_type_dropdown.enabled = False
+
+  def enable_bank_details_fields(self):
+    self.holder_name_tx.enabled = True
+    self.account_no_tx.enabled = True
+    self.brach_name_tx.enabled = True
+    self.bank_id_tx.enabled = True
+    self.bank_name_tx.enabled = True
+    self.acccount_type_dropdown.enabled = True
+
+  def edit_bank_details_click(self, **event_args):
+    self.enable_bank_details_fields()
+    self.save_bank_details_button.visible = True
+    self.edit_bank_details_button.visible = False
+
+  def save_bank_details_click(self, **event_args):
+    user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
+    if user_profile:
+      user_profile['account_name'] = self.holder_name_tx.text
+      user_profile['account_number'] = self.account_no_tx.text
+      user_profile['account_bank_branch'] = self.brach_name_tx.text
+      user_profile['bank_id'] = self.bank_id_tx.text
+      user_profile['bank_name'] = self.bank_name_tx.text
+      user_profile['account_type'] = self.acccount_type_dropdown.selected_value
+
+    self.disable_bank_details_fields()
+    self.save_bank_details_button.visible = False
+    self.edit_bank_details_button.visible = True
     
-  
-        
-# # # profile details
-# #       self.profession_dropdown.selected_value=user_profile['cre']
-# #       self.profession_dropdown.selected_value=user_profile['qualification']
-# # profession details
-#       self.college_name_tx.text=user_profile['college_name']
-#       self.college_id_tx.text =user_profile['college_id']
-#       self.college_address_tx.text=user_profile['college_address']
-#       self.college_proof.source=user_profile['college_proof']
-
-# #bank details
-#      # profession details
-#       self.college_name_tx.text=user_profile['college_name']
-#       self.college_id_tx.text =user_profile['college_id']
-#       self.college_address_tx.text=user_profile['college_address']
-#       self.college_proof.source=user_profile['college_proof']
-
-# #employee details
-#       self.company_name_tx.text=user_profile['company_name']
-#       self.occupation_dropdown.selected_value=user_profile['occupation_type']
-#       self.employee_dropdown.selected_value=user_profile['employment_type']
-#       self.organization_dropdown.selected_value=user_profile['organization_type']
-#       self.company_address_tx.text=user_profile['company_address']
-#       self.landmark_tx.text=user_profile['company_landmark']
-#       self.company_no_tx.text=user_profile['business_no']
-#       self.annual_salary_tx.text=user_profile['annual_salary']
-#       self.salary_type_dropdown.selected_value=user_profile['salary_type']
-#       self.designation_tx.text = user_profile['designation']
-#       self.emp_id_proof.source=user_profile['emp_id_proof']
-#       self.last_six_month_proof.source=user_profile['last_six_month_bank_proof']
-
-#       options_1 = app_tables.fin_borrower_employee_type.search()
-#       option_strings_1 = [str(option['borrower_employee_type']) for option in options_1]
-#       self.employee_dropdown.items = option_strings_1
-  
-#           # Populate drop_down_2 with data from 'organization_type' column
-#       options_2 = app_tables.fin_borrower_organization_type.search()
-#       option_strings_2 = [str(option['borrower_organization_type']) for option in options_2]
-#       self.organization_dropdown.items = option_strings_2
-  
-#       options = app_tables.fin_occupation_type.search()
-#       option_strings = [str(option['occupation_type']) for option in options]
-#       self.occupation_dropdown.items = option_strings
-
-#       options = app_tables.fin_borrower_salary_type.search()
-#       option_strings = [str(option['borrower_salary_type']) for option in options]
-#       self.salary_type_dropdown.items = option_strings
-
-# # former dtails
-#       self.type_of_land_dropdown.selected_value=user_profile['land_type']
-#       self.no_of_acers.text=user_profile['total_acres']
-#       self.crop_name.text=user_profile['crop_name']
-#       self.yearly_income.text=user_profile['farmer_earnings']
-
-#       options_2 = app_tables.fin_borrower_land_type.search()
-#       option_strings_2 = [str(option['land_type']) for option in options_2]
-#       self.type_of_land_dropdown.items = option_strings_2
-
-# # business details
-#       self.business_name_tex.text=user_profile['business_name']
-#       self.business_address_tex.text=user_profile['business_add']
-#       self.business_type_dropdown.selected_value=user_profile['business_type']
-#       self.no_of_emp_dropdown.selected_value=user_profile['employees_working']
-#       self.year_of_yest_tx.text=user_profile['year_estd']
-#       self.industry_type_dropdown.selected_value=user_profile['industry_type']
-#       self.last_six_turnover_tx.text=user_profile['six_month_turnover']
-#       self.last_six_month_for_business.source=user_profile['last_six_month_bank_proof']
-#       self.din_tx.text=user_profile['din']
-#       self.cin_tx.text = user_profile['cin']
-#       self.office_address_tx.text=user_profile['registered_off_add']
-#       self.proof_varification.source=user_profile['proof_verification']
-
-#       options_1 = app_tables.fin_borrower_business_type.search()
-#       option_strings_1 = [str(option['borrower_business_type']) for option in options_1]
-#       self.business_type_dropdown.items = option_strings_1
-
-#       options_2 = app_tables.fin_borrower_no_of_employees.search()
-#       option_strings_2 = [str(option['borrower_no_of_employees']) for option in options_2]
-#       self.no_of_emp_dropdown.items = option_strings_2
-
-#       user_profile.update()
-
-
-#   # profile information
-#       borrower_details = app_tables.fin_borrower.get(customer_id=self.user_id)
-#       if borrower_details:
-#         self.credit_limit_tx.text = borrower_details['credit_limit']
-#         self.member_since_tx.text = borrower_details['borrower_since']
 
   def personal_information_click(self, **event_args):
     """This method is called when the button is clicked"""
